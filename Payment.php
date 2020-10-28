2fef42c329c672277f5252996a0c4d6c965908f1
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-10-28 14:15:41 -0300

    commit inicial

---
+++ Payment.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace RegistrationPayments;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use MapasCulturais\App;
+use MapasCulturais\Traits;
+
+/**
+ * Payment
+ *
+ * @property-read int $id
+ * 
+ * @property MapasCulturais\Entities\Registration $registration
+ * @property MapasCulturais\Entities\Opportunity $opportunity
+ * @property float $amount
+ * @property DateTime $paymentDate 
+ * @property object $metadata 
+ * @property MapasCulturais\Entities\User $createdByUser 
+ * @property DateTime $createTimestamp 
+ * @property DateTime $updateTimestamp 
+ * @property int $status 
+ * 
+ * 
+ * @ORM\Table(name="payment")
+ * @ORM\Entity
+ * @ORM\entity(repositoryClass="MapasCulturais\Repository")
+ */
+class Payment extends \MapasCulturais\Entity {
+    const STATUS_PENDING = 0;
+    const STATUS_PROCESSING = 1;
+    const STATUS_FAILED = 2;    
+    const STATUS_PAID = 10;
+    
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="SEQUENCE")
+     * @ORM\SequenceGenerator(sequenceName="payment_id_seq", allocationSize=1, initialValue=1)
+     */
+    protected $id;
+
+    /**
+     * @var \MapasCulturais\Entities\Registration
+     *
+     * @ORM\ManyToOne(targetEntity="MapasCulturais\Entities\Registration")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="registration_id", referencedColumnName="id", nullable=false, onDelete="CASCADE")
+     * })
+     */
+    protected $registration;
+
+
+    /**
+     * @var \MapasCulturais\Entities\Opportunity
+     *
+     * @ORM\ManyToOne(targetEntity="MapasCulturais\Entities\Opportunity")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="opportunity_id", referencedColumnName="id", nullable=false, onDelete="CASCADE")
+     * })
+     */
+    protected $opportunity;
+
+    /**
+     * @var string|object
+     *
+     * @ORM\Column(name="amount", type="float", nullable=false)
+     */
+    protected $amount;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(name="payment_date", type="date", nullable=false)
+     */
+    protected $paymentDate;
+
+    /**
+     * @var object
+     *
+     * @ORM\Column(name="metadata", type="json_array", nullable=false)
+     */
+    protected $metadata;
+
+
+    /**
+     * @var \MapasCulturais\Entities\User
+     *
+     * @ORM\ManyToOne(targetEntity="MapasCulturais\Entities\User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="created_by_user_id", referencedColumnName="id", nullable=true)
+     * })
+     */
+    protected $createdByUser;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="create_timestamp", type="datetime", nullable=false)
+     */
+    protected $createTimestamp;
+
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="update_timestamp", type="datetime", nullable=true)
+     */
+    protected $updateTimestamp;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="status", type="smallint", nullable=false)
+     */
+    protected $status = self::STATUS_PENDING;
+
+    function __construct()
+    {
+        parent::__construct();
+
+        $app = App::i();
+
+        if(!$this->metadata) {
+            $this->metadata = (object) [];
+        }
+    }
+
+    /**
+     * @param DateTime|string $date Se string enviar no formato YYYY-MM-DD
+     * @return void 
+     */
+    function setPaymentDate ($date) {
+        if($date instanceof \DateTime) {
+            $this->paymentDate = $date;
+        } else if (is_string($date) && preg_match('#\d{4}-\d{2}-\d{2}#', $date)) { 
+            $this->paymentDate = new \DateTime($date);
+        } else {
+            throw new \Exception('Wrong paymentDate format');
+        }
+    }
+
+    function setRegistration(\MapasCulturais\Entities\Registration $registration) {
+        $this->registration = $registration;
+        $this->opportunity = $registration->opportunity;
+    } 
+}
---
+++ Plugin.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace RegistrationPayments;
+
+use MapasCulturais\App;
+
+class Plugin extends \MapasCulturais\Plugin{
+    function _init() {
+        $app = App::i();
+
+        $driver = $app->em->getConfiguration()->getMetadataDriverImpl();
+        $driver->addPaths([__DIR__]);
+        
+        // @todo implementar cache para não fazer essa consulta a cada requisição
+        if (!$app->repo('DbUpdate')->findBy(['name' => 'create table payment'])) {
+            $conn = $app->em->getConnection();
+            $conn->beginTransaction();
+
+            $conn->executeQuery("CREATE SEQUENCE payment_id_seq INCREMENT BY 1 MINVALUE 1 START 1");
+            $conn->executeQuery("
+                CREATE TABLE payment (
+                    id INT NOT NULL, 
+                    registration_id INT NOT NULL, 
+                    opportunity_id INT NOT NULL, 
+                    created_by_user_id INT DEFAULT NULL, 
+                    amount DOUBLE PRECISION NOT NULL, 
+                    payment_date DATE NOT NULL, 
+                    metadata JSON DEFAULT '{}' NOT NULL, 
+                    create_timestamp TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, 
+                    update_timestamp TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, 
+                    status SMALLINT NOT NULL, 
+                PRIMARY KEY(id))");
+            $conn->executeQuery("CREATE INDEX IDX_6D28840D833D8F43 ON payment (registration_id)");
+            $conn->executeQuery("CREATE INDEX IDX_6D28840D9A34590F ON payment (opportunity_id)");
+            $conn->executeQuery("CREATE INDEX IDX_6D28840D7D182D95 ON payment (created_by_user_id)");
+            $conn->executeQuery("COMMENT ON COLUMN payment.metadata IS '(DC2Type:json_array)'");
+            $conn->executeQuery("ALTER TABLE payment ADD CONSTRAINT FK_6D28840D833D8F43 FOREIGN KEY (registration_id) REFERENCES registration (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE");
+            $conn->executeQuery("ALTER TABLE payment ADD CONSTRAINT FK_6D28840D9A34590F FOREIGN KEY (opportunity_id) REFERENCES opportunity (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE");
+            $conn->executeQuery("ALTER TABLE payment ADD CONSTRAINT FK_6D28840D7D182D95 FOREIGN KEY (created_by_user_id) REFERENCES usr (id) NOT DEFERRABLE INITIALLY IMMEDIATE");
+
+            $app->disableAccessControl();
+            $db_update = new \MapasCulturais\Entities\DbUpdate;
+            $db_update->name = 'create table payment';
+            $db_update->save(true);
+            $app->enableAccessControl();
+            $conn->commit();
+        }
+
+    }
+
+    function register () {
+        
+    }
+}

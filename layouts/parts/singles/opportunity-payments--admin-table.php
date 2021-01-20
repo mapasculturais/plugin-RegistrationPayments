eff41212a7dd1653f55b5c8ab33c077dad8266ac
Everaldo Matias <matiaseveraldo@gmail.com>
2021-01-20 15:47:15 -0300

    Ajustes CSS nos campos de edicao em massa dos pagamentos

--- layouts/parts/singles/opportunity-payments--admin-table.php
+++ layouts/parts/singles/opportunity-payments--admin-table.php
@@ -16,7 +16,7 @@ use MapasCulturais\i;
             <input ng-model="data.search" id="search" ng-keyup="search()" placeholder="<?php i::_e("Busque pelo número de inscrição"); ?>" class="ng-pristine ng-untouched ng-valid ng-empty"> <br>
         </div>
         <div class="right">
-            <span class="label"><?php i::_e("Filtrar por data"); ?></span>
+            <span class="label"><?php i::_e("Filtrar por data:"); ?></span>
             <input type="date" ng-model="data.filterDate" placeholder="<?php i::_e("Busque pela data do pagamento"); ?>">
         </div>
     </div>
@@ -133,7 +133,7 @@ use MapasCulturais\i;
             </div>
             
             <div>
-                <label ng-model="data.editPayment.amount"><?php i::_e("Status"); ?></label>
+                <label ng-model="data.editPayment.status"><?php i::_e("Status"); ?></label>
                 <select ng-model="data.editPayment.status">
                     <option value="">Selecione</option>
                     <option value="0" ng-selected="data.editPayment.status === 0"><?php i::_e("Pendente"); ?></option>
@@ -164,17 +164,31 @@ use MapasCulturais\i;
         </header>
         
         <div class="fields">
-            <input type="date" ng-model="data.editMultiplePayments.payment_date"  value=""/>
-            <?php i::_e("R$"); ?> <input type="text" ng-model="data.editMultiplePayments.amount" placeholder="ex.: 3000,00"/>
-            <select ng-model="data.editMultiplePayments.status">
-                <option value="">Selecione</option>
-                <option value="0" ng-selected="data.editMultiplePayments.status === 0"><?php i::_e("Pendente"); ?></option>
-                <option value="1" ng-selected="data.editMultiplePayments.status === 1"><?php i::_e("Em processo"); ?></option>
-                <option value="2" ng-selected="data.editMultiplePayments.status === 2"><?php i::_e("Falha"); ?></option>
-                <option value="3" ng-selected="data.editMultiplePayments.status === 3"><?php i::_e("Exportado"); ?></option>
-                <option value="8" ng-selected="data.editMultiplePayments.status === 8"><?php i::_e("Disponível"); ?></option>
-                <option value="10" ng-selected="data.editMultiplePayments.status === 10"><?php i::_e("Pago"); ?></option>
-            </select>
+            
+            <div>
+                <label ng-model="data.editMultiplePayments.payment_date"><?php i::_e("Data"); ?></label>
+                <input type="date" ng-model="data.editMultiplePayments.payment_date" value="" />
+            </div>
+
+            <div>
+                <label ng-model="data.editMultiplePayments.amount"><?php i::_e("Valor"); ?></label>
+                <div>
+                    <span class="prefix"><?php i::_e("R$"); ?></span> <input type="text" ng-model="data.editMultiplePayments.amount" placeholder="<?php i::_e("ex.: 3000,00"); ?>"/>
+                </div>
+            </div>
+
+            <div>
+                <label ng-model="data.editMultiplePayments.status"><?php i::_e("Status"); ?></label>
+                <select ng-model="data.editMultiplePayments.status">
+                    <option value=""><?php i::_e("Selecione"); ?></option>
+                    <option value="0" ng-selected="data.editMultiplePayments.status === 0"><?php i::_e("Pendente"); ?></option>
+                    <option value="1" ng-selected="data.editMultiplePayments.status === 1"><?php i::_e("Em processo"); ?></option>
+                    <option value="2" ng-selected="data.editMultiplePayments.status === 2"><?php i::_e("Falha"); ?></option>
+                    <option value="3" ng-selected="data.editMultiplePayments.status === 3"><?php i::_e("Exportado"); ?></option>
+                    <option value="8" ng-selected="data.editMultiplePayments.status === 8"><?php i::_e("Disponível"); ?></option>
+                    <option value="10" ng-selected="data.editMultiplePayments.status === 10"><?php i::_e("Pago"); ?></option>
+                </select>
+            </div>
         </div>
         
         <div>

9b5d4949a79a9dc928dd7dcebd9e7540d1f58d36
erleibiazzio <erleibiazzio@gmail.com>
2023-12-18 18:33:54 -0300

    Chama o componente opportunity-payment-table na aba de pagamentos Ref.: #14

--- layouts/parts/payments/opportunity-payments-tab.php
+++ layouts/parts/payments/opportunity-payments-tab.php
@@ -10,9 +10,11 @@ use MapasCulturais\i;
 $this->import('
     mc-tab
     create-payment
+    opportunity-payment-table
 ');
 ?>
 
 <mc-tab label="<?= i::__('Pagamentos') ?>" slug="payment">
     <create-payment :entity="entity"></create-payment>
+    <opportunity-payment-table :entity="entity"></opportunity-payment-table>
 </mc-tab>

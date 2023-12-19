af02d9d25598e38430671f705cc3de9f541feacf
erleibiazzio <erleibiazzio@gmail.com>
2023-12-19 16:34:37 -0300

    Passa o componente de criação de pagamento para dentro o opportunity-payment-table Ref.: #15

--- components/opportunity-payment-table/template.php
+++ components/opportunity-payment-table/template.php
@@ -8,11 +8,16 @@
 use MapasCulturais\i;
 
 $this->import('
+    create-payment
     entity-table
 ');
 ?>
 
-<entity-table type="payment" :select="select" :query="query" :headers= "headers" endpint  required="registration" visible="registration,paymentDate,amount,status" >
+<entity-table type="payment" :select="select" :query="query" :headers= "headers" endpint  required="registration" visible="registration,paymentDate,amount,status">
+    <template #actions-table="{entities}" >
+            <create-payment :entity="entity" :entities="entities"></create-payment>
+    </template>
+    
     <template #paymentDate="entity" >
             {{entity.paymentDate.date('numeric year')}}
     </template>
@@ -24,6 +29,5 @@ $this->import('
     <template #amount="entity" >
             {{amountToString(entity.amount)}}
     </template>
-
    
 </entity-table>
--- layouts/parts/payments/opportunity-payments-tab.php
+++ layouts/parts/payments/opportunity-payments-tab.php
@@ -9,13 +9,11 @@ use MapasCulturais\i;
 
 $this->import('
     mc-tab
-    create-payment
     opportunity-payment-table
 ');
 ?>
 <div class="payment-tab__container">
     <mc-tab label="<?= i::__('Pagamentos') ?>" slug="payment">
-        <create-payment :entity="entity"></create-payment>
         <opportunity-payment-table :entity="entity"></opportunity-payment-table>
     </mc-tab>
 </div>

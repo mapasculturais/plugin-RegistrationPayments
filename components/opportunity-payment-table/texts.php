9348ef9388079ced99e0288bed9f539f7fad365f
erleibiazzio <erleibiazzio@gmail.com>
2023-12-21 19:37:05 -0300

    Cria internacionalização dos resultados do método setStatus Ref.: #15

--- components/opportunity-payment-table/texts.php
+++ components/opportunity-payment-table/texts.php
@@ -3,4 +3,6 @@ use MapasCulturais\i;
 
 return [
     'currency' => i::__('BRL'),
+    'setStatusSuccess' => i::__('Status do pagamento alterado com sucesso'),
+    'setStatusError' => i::__('Erro ao alterar o status do pagamento'),
 ];

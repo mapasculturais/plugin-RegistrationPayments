4f82f7da83da73947be34da4beb91c9e96af3b93
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-01-02 17:32:25 -0300

    Ajustes e estilização das modais de extração do CNAB e criação de pagamento (Ref.: #21)

--- components/create-payment/template.php
+++ components/create-payment/template.php
@@ -9,72 +9,51 @@ use MapasCulturais\i;
 
 $this->import('
     mc-modal
+    mc-icon
 ');
 ?>
-<div>
+<div class="create-payment">
     <mc-modal title="<?= i::__('Criar pagamentos:') ?>">
-        <span v-if="hasErrors">
-            <template v-for="item in response?.data">
-                <p class="field__error">* {{item}}</p>
-            </template>
-        </span>
-
         <template #actions="modal">
             <button class="button button--primary" @click="save(modal)"><?= i::__('Salvar') ?></button>
             <button class="button button--text button--text-del" @click="modal.close()"><?= i::__('cancelar') ?></button>
         </template>
-        <div>
-            <div>
-                <label><?= i::__('Inscrições') ?></label>
-            </div>
-            <div>
-                <textarea v-model="payment.registration_id" name="" id="" cols="30" rows="10"></textarea>
-            </div>
-        </div>
 
-        <div>
-            <div>
-                <div>
-                    <label><?= i::__('Previsão de pagamento') ?></label>
+        <template #default>
+            <div class="grid-12">
+                <span v-if="hasErrors" class="col-12">
+                    <p v-for="item in response?.data" class="field__error">* {{item}}</p>
+                </span>
+                <div class="col-12">
+                    <?= i::__('Agora, defina o título e os dados a serem exibidos no gráfico:') ?>
                 </div>
-                <div>
-                    <input v-model="payment.payment_date" type="date">
+                <div class="field col-12">
+                    <label for="regId"><?= i::__('Inscrições') ?></label>
+                    <textarea v-model="payment.registration_id" name="regId" id="" cols="30" rows="10"></textarea>
                 </div>
-            </div>
-            <div>
-                <div>
-                    <label><?= i::__('Valor') ?></label>
+                <div class="field col-12">
+                    <label for="paymentDate"><?= i::__('Previsão de pagamento') ?></label>
+                    <input v-model="payment.payment_date" name="paymentDate" type="date">
                 </div>
-                <div>
-                    R$<input v-model="payment.amount" v-maska data-maska="9 99#,##" data-maska-tokens="9:[0-9]:repeated" data-maska-reversed type="text">
+                <div class="field col-6">
+                    <label for="paymentAmount"><?= i::__('Valor') ?></label>
+                    <input name="paymentAmount" v-model="payment.amount" v-maska data-maska="9 99#,##" data-maska-tokens="9:[0-9]:repeated" data-maska-reversed type="text">
                 </div>
-            </div>
-            <div>
-                <div>
-                    <label><?= i::__('Status') ?></label>
-
-                </div>
-                <div>
-
-                    <select>
+                <div class="field col-6">
+                    <label for="paymentStatus"><?= i::__('Status') ?></label>
+                    <select name="paymentStatus">
                         <option v-for="item in status" :value="item.value">{{item.label}}</option>
                     </select>
                 </div>
-            </div>
-            <div>
-                <div>
-                    <div>
-                        <label><?= i::__('Observações') ?></label>
-                    </div>
-                    <div>
-                        <textarea v-model="payment.metadata.csv_line.OBSERVACOES" name="" id="" cols="30" rows="10"></textarea>
-                    </div>
+                <div class="field col-12">
+                    <label for="obs"><?= i::__('Observações') ?></label>
+                    <textarea v-model="payment.metadata.csv_line.OBSERVACOES" name="obs" id="" cols="30" rows="10"></textarea>
                 </div>
             </div>
-        </div>
+        </template>
 
         <template #button="modal">
-            <button type="button" @click="modal.open()" class="button button--primary-outline"><?= i::__('Adicionar pagamento') ?></button>
+            <button type="button" @click="modal.open()" class="button button--primary button--icon"><mc-icon name="add"></mc-icon><?= i::__('Adicionar pagamento') ?></button>
         </template>
     </mc-modal>
 </div>
--- components/extraction-cnab/template.php
+++ components/extraction-cnab/template.php
@@ -9,59 +9,45 @@ use MapasCulturais\i;
 
 $this->import('
     mc-modal
+    mc-icon
 ');
 ?>
 
-<div>
+<div class="extraction-cnab">
     <mc-modal title="<?= i::__('Exportar TXT CNAB') ?>">
-        <div>
-            <div>
-                <div>
-                    <label> <?= i::__('Identificação do lote de pagamento') ?></label>
-                </div>
-                <div>
-                    <input v-model="cnabData.identifier" type="text">
-                </div>
+        <div class="grid-12">
+            <div class="field col-12">
+                <label> <?= i::__('Identificação do lote de pagamento') ?></label>
+                <input v-model="cnabData.identifier" type="text">
             </div>
 
-            <div>
-                <div>
-                    <label><?= i::__('Data de pagamento') ?></label>
-                </div>
-                <div>
-                    <input v-model="cnabData.paymentDate" type="date">
-                </div>
+            <div class="field col-6">
+                <label><?= i::__('Data de pagamento') ?></label>
+                <input v-model="cnabData.paymentDate" type="date">
             </div>
 
-            <div>
-                <div>
-                    <label><?= i::__('Tipo de exportação') ?></label>
-                </div>
-                <div>
-                    <select v-model="cnabData.lotType">
-                        <option value="1"><?= i::__('Corrente BB') ?></option>
-                        <option value="2"><?= i::__('Poupança BB') ?></option>
-                        <option value="3"><?= i::__('Outros Bancos') ?></option>
-                    </select>
-                </div>
+            <div class="field col-6">
+                <label><?= i::__('Tipo de exportação') ?></label>
+                <select v-model="cnabData.lotType">
+                    <option value="1"><?= i::__('Corrente BB') ?></option>
+                    <option value="2"><?= i::__('Poupança BB') ?></option>
+                    <option value="3"><?= i::__('Outros Bancos') ?></option>
+                </select>
             </div>
 
-            <div>
-                <div>
-                    <label><?= i::__('Filtrar somente as Inscrições') ?></label>
-                </div>
-                <div>
-                    <textarea v-model="cnabData.registrationFilter"></textarea>
-                </div>
+            <div class="field col-12">
+                <label><?= i::__('Filtrar somente as Inscrições') ?></label>
+                <textarea v-model="cnabData.registrationFilter"></textarea>
             </div>
 
-            <div>
-                <div>
+            <div class="field col-12">
+                <label>
                     <input v-model="cnabData.ts_lot" type="checkbox">
-                    <label><?= i::__('Exportar lote de teste') ?></label>
-                </div>
+                    <?= i::__('Exportar lote de teste') ?>
+                </label>
             </div>
-            <div>
+
+            <div class="col-12">
                 <small><?= i::__('OBS: caso a data de pagamento não for informada, sera inserido a data que esta cadastrada junto aos dados de pagamento') ?></small>
             </div>
         </div>
@@ -72,7 +58,7 @@ $this->import('
         </template>
 
         <template #button="modal">
-            <button type="button" @click="modal.open()" class="button button--primary-outline"><?= i::__('Exportar TXT CNAB') ?></button>
+            <button type="button" @click="modal.open()" class="button button--primary button--icon"><?= i::__('Extrair CNAB') ?> <mc-icon name="download"></mc-icon></button>
         </template>
     </mc-modal>
 </div>

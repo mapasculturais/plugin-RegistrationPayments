b54a79f1a582155da1d886975159b55d336cc304
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-01-15 12:30:36 -0300

    Importa componente mc-select na tabela de pagamentos (Ref.: #20)

--- components/opportunity-payment-table/template.php
+++ components/opportunity-payment-table/template.php
@@ -11,14 +11,15 @@ use RegistrationPayments\Plugin;
 $this->import('
     change-history
     create-payment
-    entity-table
     entity-files-list
+    entity-table
     export-filters-spreadsheet
     extraction-cnab
-    payment-spreadsheet
     mc-confirm-button
     mc-icon
     mc-modal
+    mc-select
+    payment-spreadsheet
 ');
 
 $plugin = Plugin::getInstance();
@@ -27,117 +28,119 @@ $entity = $this->controller->requestedEntity;
 $url = $app->createUrl('payment', 'export');
 ?>
 
-<entity-table type="payment" :select="select" :query="query" :headers="headers" endpint required="registration,options" visible="registration,paymentDate,amount,status,options">
-    
-    <template #title>
-        <h3 class="bold"><?= i::__('Pagamentos') ?></h3>
-    </template>
+<div class="opportunity-payment-table">
+    <entity-table type="payment" :select="select" :query="query" :headers="headers" endpint required="registration,options" visible="registration,paymentDate,amount,status,options">
+        
+        <template #title>
+            <h3 class="bold"><?= i::__('Pagamentos') ?></h3>
+        </template>
 
-    <template #actions="{entities}">
-        <div class="opportunity-payment-table__actions">
-            <h4 class="bold"><?= i::__('Ações:') ?></h4>
+        <template #actions="{entities}">
+            <div class="opportunity-payment-table__actions">
+                <h4 class="bold"><?= i::__('Ações:') ?></h4>
 
-            <div class="opportunity-payment-table__actions grid-12">
-                <create-payment class="col-4 sm:col-12" :entity="opportunity" :entities="entities"></create-payment>
-                
-                <payment-spreadsheet class="col-4 sm:col-12" :entity="opportunity"></payment-spreadsheet>
-                
-                <?php if($cnab_enabled($entity)):  ?>
-                    <extraction-cnab class="col-4 sm:col-12" :entity="opportunity"></extraction-cnab>
-                <?php endif ?>
-            </div>
-        </div>
-    </template>
-
-    <template #advanced-actions="{entities}">
-        <div class="grid-12">
-            <div class="col-6">
-                <entity-files-list :entity="opportunity" group="export-financial-validator-files" title="<?= i::__('Validador Financeiro') ?>"></entity-files-list>
-            </div>
-        </div>
-    </template>
-
-    <template #filters="{entities}">
-        <div class="grid-12">
-            <div class="field field--horizontal col-6 sm:col-12">
-                <label><?= i::__('Data inicial')?></label>
-                <input v-model="filters.paymentFrom" @change="change($event,entities)" type="date">
-            </div>
-            <div class="field field--horizontal col-6 sm:col-12">
-                <label><?= i::__('Data final')?></label>
-                <input v-model="filters.paymentTo" @change="change($event,entities)" type="date">
-            </div>
-        </div>
-    </template>
-    
-    <template #advanced-filters="{entities}">
-        <div class="field">
-            <label><?= i::__('Status')?></label>
-            <div class="field__group">
-                <label class="field__checkbox" v-for="status in statusList">
-                    <input :checked="filters.status?.includes(status.value)" type="checkbox" :value="status.value" @input="statusFilter($event,entities)"> {{status.label}} 
-                </label>
-            </div>
-        </div>
-    </template>
-
-   <template #status="entity">
-        <mc-select :default-value="entity.status" @change-option="setStatus($event, {entity})">
-            <option v-for="item in statusList" :value="item.value">{{item.label}}</option>
-        </mc-select>
-    </template>
-
-    <template #amount="{entity}">
-        {{amountToString(entity.amount)}}
-    </template>
-
-    <template #options="{entity, refresh}">
-        <div class="opportunity-payment-table__table-actions">
-            <change-history :entity="entity"></change-history>
-
-            <mc-modal button-label="abrir" :title="'<?= i::__('Editar pagamento da inscrição') ?> ' + entity.registration.number">
-                <div class="grid-12">
-                    <div class="field col-12">
-                        <label><?= i::__('Previsão de pagamento') ?></label>
-                        <input v-model="entity.__originalValues.paymentDate" type="date">
-                    </div>
-
-                    <div class="field col-12">
-                        <label><?= i::__('Valor') ?></label>
-                        <span class="field__currence">
-                            <span class="field__currence-sign">R$</span>
-                            <input v-model="amount" v-maska data-maska="9 99#,##" data-maska-tokens="9:[0-9]:repeated" data-maska-reversed type="text">
-                        </span> 
-                    </div>
-
-                    <div class="field col-12">
-                        <label><?= i::__('Observações') ?></label>
-                        <textarea v-model="entity.metadata.csv_line.OBSERVACOES" name="" id="" cols="30" rows="5"></textarea>
-                    </div>
+                <div class="opportunity-payment-table__actions grid-12">
+                    <create-payment class="col-4 sm:col-12" :entity="opportunity" :entities="entities"></create-payment>
+                    
+                    <payment-spreadsheet class="col-4 sm:col-12" :entity="opportunity"></payment-spreadsheet>
+                    
+                    <?php if($cnab_enabled($entity)):  ?>
+                        <extraction-cnab class="col-4 sm:col-12" :entity="opportunity"></extraction-cnab>
+                    <?php endif ?>
                 </div>
+            </div>
+        </template>
 
-                <template #button={open}>
-                    <mc-icon name="edit" @click="editPayment(open, entity)"></mc-icon>
-                </template>
+        <template #advanced-actions="{entities}">
+            <div class="grid-12">
+                <div class="col-6">
+                    <entity-files-list :entity="opportunity" group="export-financial-validator-files" title="<?= i::__('Validador Financeiro') ?>"></entity-files-list>
+                </div>
+            </div>
+        </template>
 
-                <template #actions="modal">
-                    <button class="button button" @click="modal.close()"><?= i::__('Cancelar') ?></button>
-                    <button class="button button--primary" @click="updatePayment(entity, refresh)"><?= i::__('Salvar') ?></button>
-                </template>
-            </mc-modal>
+        <template #filters="{entities}">
+            <div class="grid-12">
+                <div class="field field--horizontal col-6 sm:col-12">
+                    <label><?= i::__('Data inicial')?></label>
+                    <input v-model="filters.paymentFrom" @change="change($event,entities)" type="date">
+                </div>
+                <div class="field field--horizontal col-6 sm:col-12">
+                    <label><?= i::__('Data final')?></label>
+                    <input v-model="filters.paymentTo" @change="change($event,entities)" type="date">
+                </div>
+            </div>
+        </template>
+        
+        <template #advanced-filters="{entities}">
+            <div class="field">
+                <label><?= i::__('Status')?></label>
+                <div class="field__group">
+                    <label class="field__checkbox" v-for="status in statusList">
+                        <input :checked="filters.status?.includes(status.value)" type="checkbox" :value="status.value" @input="statusFilter($event,entities)"> {{status.label}} 
+                    </label>
+                </div>
+            </div>
+        </template>
 
-            <mc-confirm-button @confirm="delPayment(entity, refresh)">
-                <template #button="{open}">
-                    <mc-icon name="trash" @click="open()"></mc-icon>
-                </template> 
-                <template #message="message">
-                    <?php i::_e('Deseja deletar o pagamento?') ?>
-                </template> 
-            </mc-confirm-button>
-        </div>
-    </template>
-</entity-table>
+    <template #status="entity">
+            <mc-select :default-value="entity.status" @change-option="setStatus($event, {entity})">
+                <option v-for="item in statusList" :value="item.value">{{item.label}}</option>
+            </mc-select>
+        </template>
 
-<div class="opportunity-payment-table__download-btn">
-    <export-filters-spreadsheet :entity="opportunity" :filters="filters"></export-filters-spreadsheet>
+        <template #amount="{entity}">
+            {{amountToString(entity.amount)}}
+        </template>
+
+        <template #options="{entity, refresh}">
+            <div class="opportunity-payment-table__table-actions">
+                <change-history :entity="entity"></change-history>
+
+                <mc-modal button-label="abrir" :title="'<?= i::__('Editar pagamento da inscrição') ?> ' + entity.registration.number">
+                    <div class="grid-12">
+                        <div class="field col-12">
+                            <label><?= i::__('Previsão de pagamento') ?></label>
+                            <input v-model="entity.__originalValues.paymentDate" type="date">
+                        </div>
+
+                        <div class="field col-12">
+                            <label><?= i::__('Valor') ?></label>
+                            <span class="field__currence">
+                                <span class="field__currence-sign">R$</span>
+                                <input v-model="amount" v-maska data-maska="9 99#,##" data-maska-tokens="9:[0-9]:repeated" data-maska-reversed type="text">
+                            </span> 
+                        </div>
+
+                        <div class="field col-12">
+                            <label><?= i::__('Observações') ?></label>
+                            <textarea v-model="entity.metadata.csv_line.OBSERVACOES" name="" id="" cols="30" rows="5"></textarea>
+                        </div>
+                    </div>
+
+                    <template #button={open}>
+                        <mc-icon name="edit" @click="editPayment(open, entity)"></mc-icon>
+                    </template>
+
+                    <template #actions="modal">
+                        <button class="button button" @click="modal.close()"><?= i::__('Cancelar') ?></button>
+                        <button class="button button--primary" @click="updatePayment(entity, refresh)"><?= i::__('Salvar') ?></button>
+                    </template>
+                </mc-modal>
+
+                <mc-confirm-button @confirm="delPayment(entity, refresh)">
+                    <template #button="{open}">
+                        <mc-icon name="trash" @click="open()"></mc-icon>
+                    </template> 
+                    <template #message="message">
+                        <?php i::_e('Deseja deletar o pagamento?') ?>
+                    </template> 
+                </mc-confirm-button>
+            </div>
+        </template>
+    </entity-table>
+
+    <div class="opportunity-payment-table__download-btn">
+        <export-filters-spreadsheet :entity="opportunity" :filters="filters"></export-filters-spreadsheet>
+    </div>
 </div>

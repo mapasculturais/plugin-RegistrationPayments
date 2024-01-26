a9de18f314486adcf0f99f541d7e13cb8c9b7f86
Raul Vitor Lucena Brito <raulvitorcc@gmail.com>
2024-01-26 13:10:16 -0300

    Ajusta modal do histórico de alterações (Ref.: #20)

--- components/change-history/template.php
+++ components/change-history/template.php
@@ -14,29 +14,47 @@ $this->import('
 ?>
 <div class="change-history">
     <mc-modal button-label="abrir" :title="'<?= i::__('Histórico de alterações:') ?>'">
-        <div class="grid-12">
-            <div class="field col-6">
-                <h3><?= i::__('Revisões') ?></h3>
-                <div v-for="(revision, id) in revisions.revisions" :key="id" @click="getDataRevision(revision.id, revision.user.profile.name)">
-                    <span><b>{{ formatMessage(revision) }} <?= i::__('em') ?> {{ formatDate(revision.createTimestamp.date) }}</b></span>
-                    <br/>
-                    <span><?= i::__('Por') ?>: {{ revision.user.profile.name }}</span>
+        <div class="grid-12 change-history__modal-content">
+            <div class="change-history__reviews col-6 sm:col-12">
+                <p class="bold"><?= i::__('Revisões') ?></p>
+
+                <div class="change-history__all-reviews mc-scroll">
+                    <div class="change-history__individual-review" v-for="(revision, id) in revisions.revisions" :key="id" @click="getDataRevision(revision.id, revision.user.profile.name)">
+                        <span><b>{{ formatMessage(revision) }} <?= i::__('em') ?> {{ formatDate(revision.createTimestamp.date) }}</b></span>
+                        <span><?= i::__('Por') ?>: {{ revision.user.profile.name }}</span>
+                    </div>
                 </div>
             </div>
 
-            <div class="field col-6">
-                <h3><?= i::__('Detalhes') ?></h3>
-                <div v-if="dataRevision && dataRevision.amount">
-                    <div>
-                        <div><?= i::__('Por') ?>: {{dataRevision.agent}}</div>
-                        <div><?= i::__('Data') ?>: {{dataRevision.date.date('numeric year')}} {{dataRevision.date.time('numeric')}}</div>
-                        <div><?= i::__('Valor') ?>: {{amountToString(dataRevision.amount)}}</div>
-                        <div><?= i::__('Status') ?>: {{showStatus(dataRevision.status)}}</div>
-                        <div><?= i::__('Observação') ?>: {{dataRevision.observation}}</div>
-                    </div>
-                </div>
-                <div v-else>
-                    <?= i::__('Selecione uma revisão para visualizar os detalhes.') ?>
+            <div class="change-history__details col-6 sm:col-12">
+                <p class="bold"><?= i::__('Detalhes') ?></p>
+
+                <div class="change-history__review-details mc-scroll">
+                    <template v-if="dataRevision && dataRevision.amount">
+                        <div class="change-history__review-item">
+                            <strong><?= i::__('Por') ?>:</strong> {{dataRevision.agent}}
+                        </div>
+
+                        <div class="change-history__review-item">
+                            <strong><?= i::__('Data') ?>:</strong> {{dataRevision.date.date('numeric year')}} {{dataRevision.date.time('numeric')}}
+                        </div>
+
+                        <div class="change-history__review-item">
+                            <strong><?= i::__('Valor') ?>:</strong> {{amountToString(dataRevision.amount)}}
+                        </div>
+
+                        <div class="change-history__review-item">
+                            <strong><?= i::__('Status') ?>:</strong> {{showStatus(dataRevision.status)}}
+                        </div>
+
+                        <div v-if="dataRevision.observation" class="change-history__review-item">
+                            <strong><?= i::__('Observação') ?>:</strong> {{dataRevision.observation}}
+                        </div>
+                    </template>
+
+                    <template v-else>
+                        <?= i::__('Selecione uma revisão para visualizar os detalhes.') ?>
+                    </template>
                 </div>
             </div>
         </div>

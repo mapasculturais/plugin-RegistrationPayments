619a50ba83a3adc254af811c70ae77cc9e779026
israelmelo <israelg.8@gmail.com>
2024-06-06 16:22:09 -0300

    Criado botão "Enviar" para deixar os campos disabled caso já tenha sido enviado os dados Ref.: #32

--- components/registration-payment-tab/script.js
+++ components/registration-payment-tab/script.js
@@ -19,27 +19,22 @@ app.component('registration-payment-tab', {
 
     data() {
         return {
-
+            dataSent: false,
         }
     },
 
     methods: {
-        async validate() {
-            const messages = useMessages();
-            try {
-                await this.entity.save();
-                const success = await this.entity.POST('validateEntity', {});
-                if (success) {
-                    messages.success(this.text('Validado'));
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        },
-
         sendPaymentData() { 
             this.entity.payment_sent_timestamp = new Date();
             this.entity.save();
+
+            this.dataSent = true;
         }
     },
+
+    mounted() {
+        if(this.entity.payment_sent_timestamp !== null) {
+            this.dataSent = true;
+        }
+    }
 });
--- components/registration-payment-tab/template.php
+++ components/registration-payment-tab/template.php
@@ -20,16 +20,28 @@ $this->import('
             <h3><?= i::__('Dados do proponente') ?></h3>
 
             <div class="grid-12">
-
-                <div class="col-12">
-                    <entity-field :entity="entity" prop="payment_proponent_name" :autosave="3000"></entity-field>
+                <div v-if="!dataSent" class="col-12">
+                    <entity-field :entity="entity" prop="payment_proponent_name"></entity-field>
                 </div>
-                <div class="col-6">
-                    <entity-field :entity="entity" prop="payment_social_type" :autosave="3000"></entity-field>
+                <div v-else class="col-12 field">
+                    <label class="field__title" for="payment_proponent_name"><?= i::__('Nome do proponente') ?></label>
+                    <input id="payment_proponent_name" :value="entity.payment_proponent_name" type="text" autocomplete="off" disabled>
                 </div>
 
-                <div class="col-6">
-                    <entity-field :entity="entity" prop="payment_proponent_document" :autosave="3000" mask="['###.###.###-##', '##.###.###/####-##']"></entity-field>
+                <div v-if="!dataSent" class="col-6">
+                    <entity-field :entity="entity" prop="payment_social_type"></entity-field>
+                </div>
+                <div v-else class="col-6 field">
+                    <label class="field__title" for="payment_social_type"><?= i::__('Tipo social') ?></label>
+                    <input id="payment_social_type" :value="entity.payment_social_type" type="text" autocomplete="off" disabled>
+                </div>
+
+                <div v-if="!dataSent" class="col-6">
+                    <entity-field :entity="entity" prop="payment_proponent_document" mask="['###.###.###-##', '##.###.###/####-##']"></entity-field>
+                </div>
+                <div v-else class="col-6 field">
+                    <label class="field__title" for="payment_proponent_document"><?= i::__('Documento do proponente') ?></label>
+                    <input id="payment_proponent_document" :value="entity.payment_proponent_document" type="text" autocomplete="off" disabled>
                 </div>
             </div>
         </mc-card>
@@ -38,30 +50,56 @@ $this->import('
             <h3><?= i::__('Dados bancários do proponente') ?></h3>
 
             <div class="grid-12">
-                <!-- Faze v-if para todos os campos e desativar o botão enviar -->
-                <div class="col-12">
-                    <entity-field :entity="entity" prop="payment_account_type" :autosave="3000"></entity-field>
+                <div v-if="!dataSent" class="col-12">
+                    <entity-field :entity="entity" prop="payment_account_type"></entity-field>
+                </div>
+                <div v-else class="col-12 field">
+                    <label class="field__title" for="payment_account_type"><?= i::__('Tipo de conta') ?></label>
+                    <input id="payment_account_type" :value="entity.payment_account_type" type="text" autocomplete="off" disabled>
                 </div>
 
-                <div class="col-12">
-                    <entity-field :entity="entity" prop="payment_bank" :autosave="3000"></entity-field>
+                <div v-if="!dataSent" class="col-12">
+                    <entity-field :entity="entity" prop="payment_bank"></entity-field>
+                </div>
+                <div v-else class="col-12 field">
+                    <label class="field__title" for="payment_bank"><?= i::__('Banco') ?></label>
+                    <input id="payment_bank" :value="entity.payment_bank" type="text" autocomplete="off" disabled>
                 </div>
 
-                <div class="col-6">
-                    <entity-field :entity="entity" prop="payment_branch" :autosave="3000"></entity-field>
+                <div v-if="!dataSent" class="col-6">
+                    <entity-field :entity="entity" prop="payment_branch"></entity-field>
                 </div>
-                <div class="col-6">
-                    <entity-field :entity="entity" prop="payment_branch_dv" :autosave="3000" mask="**"></entity-field>
+                <div v-else class="col-6 field">
+                    <label class="field__title" for="payment_branch"><?= i::__('Agência sem o dígito') ?></label>
+                    <input id="payment_branch" :value="entity.payment_branch" type="text" autocomplete="off" disabled>
                 </div>
 
-                <div class="col-6">
-                    <entity-field :entity="entity" prop="payment_account" :autosave="3000"></entity-field>
-
+                <div v-if="!dataSent" class="col-6">
+                    <entity-field :entity="entity" prop="payment_branch_dv" mask="**"></entity-field>
                 </div>
-                <div class="col-6">
-                    <entity-field :entity="entity" prop="payment_account_dv" :autosave="3000"></entity-field>
+                <div v-else class="col-6 field">
+                    <label class="field__title" for="payment_branch_dv"><?= i::__('Dígito verificador da agência') ?></label>
+                    <input id="payment_branch_dv" :value="entity.payment_branch_dv" type="text" autocomplete="off" disabled>
+                </div>
+
+                <div v-if="!dataSent" class="col-6">
+                    <entity-field :entity="entity" prop="payment_account"></entity-field>
+                </div>
+                <div v-else class="col-6 field">
+                    <label class="field__title" for="payment_account"><?= i::__('Conta sem o dígito') ?></label>
+                    <input id="payment_account" :value="entity.payment_account" type="text" autocomplete="off" disabled>
+                </div>
+
+                <div v-if="!dataSent" class="col-6">
+                    <entity-field :entity="entity" prop="payment_account_dv"></entity-field>
+                </div>
+                <div v-else class="col-6 field">
+                    <label class="field__title" for="payment_account_dv"><?= i::__('Dígito verificador da conta') ?></label>
+                    <input id="payment_account_dv" :value="entity.payment_account_dv" type="text" autocomplete="off" disabled>
                 </div>
             </div>
         </mc-card>
+        
+        <button class="button button--primary" @click="sendPaymentData" :disabled="dataSent"><?= i::__('Enviar') ?></button>
     </div>
 </mc-tab>

57a76351f4d0e42cf475e290ad5c7f2ddf31e0be
erleibiazzio <erleibiazzio@gmail.com>
2024-06-07 13:15:15 -0300

    Garante a validação dos erros antes de enviar os dados de pagamento Ref.: #32

--- components/registration-payment-tab/script.js
+++ components/registration-payment-tab/script.js
@@ -19,10 +19,25 @@ app.component('registration-payment-tab', {
 
     methods: {
         sendPaymentData() { 
-            this.entity.payment_sent_timestamp = new Date();
-            this.entity.save();
-
-            this.dataSent = true;
+            const properties = {
+                payment_social_type: this.entity.social_type,
+                payment_proponent_name: this.entity.payment_proponent_name,
+                payment_proponent_document: this.entity.payment_proponent_document,
+                payment_account_type: this.entity.payment_account_type,
+                payment_bank: this.entity.payment_bank,
+                payment_branch: this.entity.payment_branch,
+                payment_branch_dv: this.entity.payment_branch_dv,
+                payment_account: this.entity.payment_account,
+                payment_account_dv: this.entity.payment_account_dv,
+                payment_sent_timestamp: this.entity.payment_sent_timestamp,
+            };
+            
+            this.entity.POST('validateProperties', {data: properties, callback:(response) => {
+                if(!response.error) {
+                    this.entity.payment_sent_timestamp = new Date();
+                    this.entity.save();
+                }
+            }})
         },
 
         formatDate(value) {

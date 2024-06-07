d408b01094b969bb9896fca459bc6a3db6de4234
erleibiazzio <erleibiazzio@gmail.com>
2024-06-06 23:14:22 -0300

    Organiza registros dos metadados Dos dados bancários e fonte pagadora

---
+++ registereds/payment_company_data.php
@@ -0,0 +1,69 @@
+<?php
+
+use MapasCulturais\i;
+
+$payment_company_data = [
+    'payment_company_data_name' => [
+        'label' => i::__('Nome ou Razão Social'),
+        'type' => 'string',
+        'validations' => [
+            'required' => \MapasCulturais\i::__("Nome ou Razão Social é obrigatório")
+        ]
+    ],
+    'payment_company_data_registration_type' => [
+        'label' => i::__('Tipo social'),
+        'type' => 'select',
+        'options' => [
+            1 => i::__('Pessoa física - CPF'),
+            2 => i::__('Pessoa jurídica - CNPJ'),
+        ],
+        'validations' => [
+            'required' => \MapasCulturais\i::__("Tipo social é obrigatório")
+        ]
+    ],
+    'payment_company_data_registration_number' => [
+        'label' => i::__('CPF/CNPJ'),
+        'type' => 'string',
+        'field_type' => 'fieldMask',
+        'validations' => [
+            'required' => \MapasCulturais\i::__("CPF/CNPJ é obrigatório")
+        ]
+    ],
+    'payment_company_data_bank' => [
+        'label' => i::__('Banco'),
+        'type' => 'string',
+        'default' => '001'
+    ],
+    'payment_company_data_branch' => [
+        'label' => i::__('Agência'),
+        'type' => 'string',
+        'validations' => [
+            'required' => \MapasCulturais\i::__("A agência agência é obrigatória")
+        ]
+    ],
+    'payment_company_data_branch_dv' => [
+        'label' => i::__('Dígito verificador da agência é obrigatório'),
+        'type' => 'string'
+    ],
+    'payment_company_data_account' => [
+        'label' => i::__('Conta'),
+        'type' => 'string',
+        'validations' => [
+            'required' => \MapasCulturais\i::__("A conta é obrigatória")
+        ]
+    ],
+    'payment_company_data_account_dv' => [
+        'label' => i::__('Dígito verificador da conta'),
+        'type' => 'string',
+        'validations' => [
+            'required' => \MapasCulturais\i::__("O dígito verificador da conta é obrigatório")
+        ]
+    ],
+    'payment_company_data_agreement' => [
+        'label' => i::__('Convênio'),
+        'type' => 'string',
+        'validations' => [
+            'required' => \MapasCulturais\i::__("O Convênio é obrigatório")
+        ]
+    ]
+];

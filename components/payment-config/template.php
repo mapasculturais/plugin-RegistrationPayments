93691f6e7e503058c8249aba18780ee8994c0072
Welington De Lima Olegario <wolegario49@gmail.com>
2024-09-05 18:23:46 -0300

    Faz a refaturação do formulario  de configuração de pagamentos Ref.: #34

--- components/payment-config/template.php
+++ components/payment-config/template.php
@@ -16,88 +16,64 @@ $this->import('
 
 ?>
 
-<li v-if="global.mcTabActive == 'config' && entity.has_payment_phase" v-for="(step, index) in steps" :class="{active: step.active}">
-    <section class="stepper-step">
-        <header :class="['stepper-header', {'open':step.active}]">
-            <div class="stepper-header__content">
-                <h3 class="info__title"><?= i::__('Fase de pagamentos') ?></h3>
-                <div class="dates">
-                    <div class="date">
-                        <div class="date__title"> <?= i::__('Data de início') ?> </div>
-                        <div v-if="entity.payment_registration_from" class="date__content">{{entity.payment_registration_from.date('2-digit year')}} {{entity.payment_registration_from.time('numeric')}}</div>
-                    </div>
-                    <div class="date">
-                        <div class="date__title"> <?= i::__('Data final') ?> </div>
-                        <div v-if="entity.payment_registration_to" class="date__content">{{entity.payment_registration_to.date('2-digit year')}} {{entity.payment_registration_to.time('numeric')}}</div>
-                    </div>
-                </div>
-            </div>
+<div class="stepper-header__content">
+    <h3 class="info__title"><?= i::__('Configuração de pagamentos') ?></h3>
+    <div class="dates">
+        <div class="date">
+            <div class="date__title"> <?= i::__('Data de início') ?> </div>
+            <div v-if="entity.payment_registration_from" class="date__content">{{entity.payment_registration_from.date('2-digit year')}} {{entity.payment_registration_from.time('numeric')}}</div>
+        </div>
+        <div class="date">
+            <div class="date__title"> <?= i::__('Data final') ?> </div>
+            <div v-if="entity.payment_registration_to" class="date__content">{{entity.payment_registration_to.date('2-digit year')}} {{entity.payment_registration_to.time('numeric')}}</div>
+        </div>
+    </div>
+</div>
+<article class="mc-card col-12">
+    <div class="grid-12">
+        <entity-field :entity="entity" prop="payment_registration_from" :autosave="3000" classes="col-6 sm:col-12"></entity-field>
+        <entity-field :entity="entity" prop="payment_registration_to" :autosave="3000" classes="col-6 sm:col-12"></entity-field>
+    </div><br />
 
-            <a class="expand-stepper" v-if="step.active" @click="step.close()"><label><?= i::__('Diminuir') ?></label><mc-icon name="arrowPoint-up"></mc-icon></a>
-            <a class="expand-stepper" v-if="!step.active" @click="step.open()"><label><?= i::__('Expandir') ?></label> <mc-icon name="arrowPoint-down"></mc-icon></a>
-        </header>
-        <main class="stepper-main grid-12" v-if="step.active">
-            <article class="mc-card col-12">
-                <div class="grid-12">
-                    <entity-field :entity="entity" prop="payment_registration_from" :autosave="3000" classes="col-6 sm:col-12"></entity-field>
-                    <entity-field :entity="entity" prop="payment_registration_to" :autosave="3000" classes="col-6 sm:col-12"></entity-field>
-                </div><br />
+    <h4 class="bold"><?= i::__('Dados da fonte pagadora') ?></h4><br>
 
-                <h4 class="bold"><?= i::__('Dados da fonte pagadora') ?></h4><br>
+    <div class="grid-12">
+        <div class="col-4">
+            <entity-field :entity="entity" prop="payment_company_data_name" :autosave="3000"></entity-field>
+        </div>
+        <div class="col-4">
+            <entity-field :entity="entity" prop="payment_company_data_registration_type" :autosave="3000"></entity-field>
+        </div>
+        <div class="col-4">
+            <entity-field :entity="entity" prop="payment_company_data_registration_number" :autosave="3000" :mask="documentMask()"></entity-field>
+        </div>
+    </div><br>
 
-                <div class="grid-12">
-                    <div class="col-4">
-                        <entity-field :entity="entity" prop="payment_company_data_name" :autosave="3000"></entity-field>
-                    </div>
-                    <div class="col-4">
-                        <entity-field :entity="entity" prop="payment_company_data_registration_type" :autosave="3000"></entity-field>
-                    </div>
-                    <div class="col-4">
-                        <entity-field :entity="entity" prop="payment_company_data_registration_number" :autosave="3000" :mask="documentMask()"></entity-field>
-                    </div>
-                </div><br>
+    <h4 class="bold"><?= i::__('Dados bancários da fonte pagadora') ?></h4><br>
+    <div class="grid-12">
+        <div class="col-6 field">
+            <label class="field__title" for="payment_company_data_bank"><?= i::__('Banco') ?></label>
+            <input id="payment_company_data_bank" value="Banco Do Brasil S.A (BB) - 1" type="text" autocomplete="off" disabled>
+        </div>
 
-                <h4 class="bold"><?= i::__('Dados bancários da fonte pagadora') ?></h4><br>
-                <div class="grid-12">
-                    <div class="col-6 field">
-                        <label class="field__title" for="payment_company_data_bank"><?= i::__('Banco') ?></label>
-                        <input id="payment_company_data_bank" value="Banco Do Brasil S.A (BB) - 1" type="text" autocomplete="off" disabled>
-                    </div>
+        <div class="col-6">
+            <entity-field :entity="entity" prop="payment_company_data_agreement" :autosave="3000"></entity-field>
+        </div>
 
-                    <div class="col-6">
-                        <entity-field :entity="entity" prop="payment_company_data_agreement" :autosave="3000"></entity-field>
-                    </div>
+        <div class="col-6">
+            <entity-field :entity="entity" prop="payment_company_data_branch" :autosave="3000"></entity-field>
+        </div>
 
-                    <div class="col-6">
-                        <entity-field :entity="entity" prop="payment_company_data_branch" :autosave="3000"></entity-field>
-                    </div>
+        <div class="col-6">
+            <entity-field :entity="entity" prop="payment_company_data_branch_dv" :autosave="3000"></entity-field>
+        </div>
 
-                    <div class="col-6">
-                        <entity-field :entity="entity" prop="payment_company_data_branch_dv" :autosave="3000"></entity-field>
-                    </div>
+        <div class="col-6">
+            <entity-field :entity="entity" prop="payment_company_data_account" :autosave="3000"></entity-field>
+        </div>
 
-                    <div class="col-6">
-                        <entity-field :entity="entity" prop="payment_company_data_account" :autosave="3000"></entity-field>
-                    </div>
-
-                    <div class="col-6">
-                        <entity-field :entity="entity" prop="payment_company_data_account_dv" :autosave="3000"></entity-field>
-                    </div>
-                </div>
-            </article>
-            <div class="phase-delete col-12">
-
-                <mc-confirm-button message="<?= i::esc_attr__('Confirma a execução da ação?') ?>" @confirm="deletePaymentPhase()">
-                    <template #button="modal">
-                        <button class="phase-delete__trash button button--text button--sm" @click="modal.open()">
-                            <div class="icon">
-                                <mc-icon name="trash" class="secondary__color"></mc-icon>
-                            </div>
-                            <h5><?= i::__("Excluir fase de pagamentos") ?></h5>
-                        </button>
-                    </template>
-                </mc-confirm-button>
-            </div>
-        </main>
-    </section>
-</li>
+        <div class="col-6">
+            <entity-field :entity="entity" prop="payment_company_data_account_dv" :autosave="3000"></entity-field>
+        </div>
+    </div>
+</article>

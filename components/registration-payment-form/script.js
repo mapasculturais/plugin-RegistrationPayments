135a1bd9da5396d9f569a507753333392bc1b5c9
erleibiazzio <erleibiazzio@gmail.com>
2024-09-13 16:37:36 -0300

    Remove códigos desnecessários

--- components/registration-payment-form/script.js
+++ components/registration-payment-form/script.js
@@ -8,16 +8,6 @@ app.component('registration-payment-form', {
         }
     },
 
-    // data() {
-    //     let paymentData = $MAPAS.config.registrationPaymentTab.paymentData;
-    //     const firstPhase = new Entity('registration', paymentData.id);
-    //     firstPhase.populate(paymentData);
-
-    //     return {
-    //         entity: firstPhase
-    //     }
-    // },
-
     computed: {
         opportunity() {
             return $MAPAS.config.registrationPaymentTab.opportunity;
@@ -30,29 +20,6 @@ app.component('registration-payment-form', {
     },
 
     methods: {
-        // saveEntity(data) {
-        //     console.log(data)
-        // },
-        // sendPaymentData() { 
-        //     const properties = {
-        //         payment_social_type: this.entity.social_type,
-        //         payment_proponent_name: this.entity.payment_proponent_name,
-        //         payment_proponent_document: this.entity.payment_proponent_document,
-        //         payment_account_type: this.entity.payment_account_type,
-        //         payment_bank: this.entity.payment_bank,
-        //         payment_branch: this.entity.payment_branch,
-        //         payment_branch_dv: this.entity.payment_branch_dv,
-        //         payment_account: this.entity.payment_account,
-        //         payment_account_dv: this.entity.payment_account_dv,
-        //     };
-            
-        //     this.entity.POST('validateProperties', {data: properties, callback:(response) => {
-        //         if(!response.error) {
-        //             this.entity.payment_sent_timestamp = new Date();
-        //             this.entity.save();
-        //         }
-        //     }})
-        // },
 
         formatDate(value) {
             if(value) {
@@ -61,7 +28,6 @@ app.component('registration-payment-form', {
             }
         },
         isEditable() {
-            return false;
             if(this.entity?.sentTimestamp == null || this.entity?.sentTimestamp == '') {
                 return true;
             }

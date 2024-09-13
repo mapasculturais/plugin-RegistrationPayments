a19ec91b7167eda1a589d13b28636b1e1917a884
Welington De Lima Olegario <wolegario49@gmail.com>
2024-09-13 17:33:28 -0300

    Faz a estilização do ambiente Ref.: #34

--- components/payment-config/template.php
+++ components/payment-config/template.php
@@ -12,68 +12,66 @@ $this->import('
     opportunity-payment-table
     mc-card
     mc-confirm-button
+    mc-icon
 ')
 
 ?>
-
-<div class="stepper-header__content">
-    <h3 class="info__title"><?= i::__('Configuração de pagamentos') ?></h3>
-    <div class="dates">
-        <div class="date">
-            <div class="date__title"> <?= i::__('Data de início') ?> </div>
-            <div v-if="entity.payment_registration_from" class="date__content">{{entity.payment_registration_from.date('2-digit year')}} {{entity.payment_registration_from.time('numeric')}}</div>
-        </div>
-        <div class="date">
-            <div class="date__title"> <?= i::__('Data final') ?> </div>
-            <div v-if="entity.payment_registration_to" class="date__content">{{entity.payment_registration_to.date('2-digit year')}} {{entity.payment_registration_to.time('numeric')}}</div>
+<div class="mc-card payment-config col-12">
+    <div class="payment-config-title stepper-header__content">
+        <h3 class=" info__title"><?= i::__('Configuração de pagamentos') ?></h3>
+        <div>
+            <button class="button button--md button--text-danger button-icon" @click="toggleConfigPayment()">
+                <mc-icon name="trash"></mc-icon>
+            </button>
         </div>
     </div>
+
+    <article class="">
+        <div class="grid-12">
+            <entity-field :entity="entity" prop="payment_registration_from" :autosave="3000" classes="col-6 sm:col-12"></entity-field>
+            <entity-field :entity="entity" prop="payment_registration_to" :autosave="3000" classes="col-6 sm:col-12"></entity-field>
+        </div><br />
+
+        <h4 class="bold"><?= i::__('Dados da fonte pagadora') ?></h4><br>
+
+        <div class="grid-12">
+            <div class="col-4">
+                <entity-field :entity="entity" prop="payment_company_data_name" :autosave="3000"></entity-field>
+            </div>
+            <div class="col-4">
+                <entity-field :entity="entity" prop="payment_company_data_registration_type" :autosave="3000"></entity-field>
+            </div>
+            <div class="col-4">
+                <entity-field :entity="entity" prop="payment_company_data_registration_number" :autosave="3000" :mask="documentMask()"></entity-field>
+            </div>
+        </div><br>
+
+        <h4 class="bold"><?= i::__('Dados bancários da fonte pagadora') ?></h4><br>
+        <div class="grid-12">
+            <div class="col-6 field">
+                <label class="field__title" for="payment_company_data_bank"><?= i::__('Banco') ?></label>
+                <input id="payment_company_data_bank" value="Banco Do Brasil S.A (BB) - 1" type="text" autocomplete="off" disabled>
+            </div>
+
+            <div class="col-6">
+                <entity-field :entity="entity" prop="payment_company_data_agreement" :autosave="3000"></entity-field>
+            </div>
+
+            <div class="col-6">
+                <entity-field :entity="entity" prop="payment_company_data_branch" :autosave="3000"></entity-field>
+            </div>
+
+            <div class="col-6">
+                <entity-field :entity="entity" prop="payment_company_data_branch_dv" :autosave="3000"></entity-field>
+            </div>
+
+            <div class="col-6">
+                <entity-field :entity="entity" prop="payment_company_data_account" :autosave="3000"></entity-field>
+            </div>
+
+            <div class="col-6">
+                <entity-field :entity="entity" prop="payment_company_data_account_dv" :autosave="3000"></entity-field>
+            </div>
+        </div>
+    </article>
 </div>
-<article class="mc-card col-12">
-    <div class="grid-12">
-        <entity-field :entity="entity" prop="payment_registration_from" :autosave="3000" classes="col-6 sm:col-12"></entity-field>
-        <entity-field :entity="entity" prop="payment_registration_to" :autosave="3000" classes="col-6 sm:col-12"></entity-field>
-    </div><br />
-
-    <h4 class="bold"><?= i::__('Dados da fonte pagadora') ?></h4><br>
-
-    <div class="grid-12">
-        <div class="col-4">
-            <entity-field :entity="entity" prop="payment_company_data_name" :autosave="3000"></entity-field>
-        </div>
-        <div class="col-4">
-            <entity-field :entity="entity" prop="payment_company_data_registration_type" :autosave="3000"></entity-field>
-        </div>
-        <div class="col-4">
-            <entity-field :entity="entity" prop="payment_company_data_registration_number" :autosave="3000" :mask="documentMask()"></entity-field>
-        </div>
-    </div><br>
-
-    <h4 class="bold"><?= i::__('Dados bancários da fonte pagadora') ?></h4><br>
-    <div class="grid-12">
-        <div class="col-6 field">
-            <label class="field__title" for="payment_company_data_bank"><?= i::__('Banco') ?></label>
-            <input id="payment_company_data_bank" value="Banco Do Brasil S.A (BB) - 1" type="text" autocomplete="off" disabled>
-        </div>
-
-        <div class="col-6">
-            <entity-field :entity="entity" prop="payment_company_data_agreement" :autosave="3000"></entity-field>
-        </div>
-
-        <div class="col-6">
-            <entity-field :entity="entity" prop="payment_company_data_branch" :autosave="3000"></entity-field>
-        </div>
-
-        <div class="col-6">
-            <entity-field :entity="entity" prop="payment_company_data_branch_dv" :autosave="3000"></entity-field>
-        </div>
-
-        <div class="col-6">
-            <entity-field :entity="entity" prop="payment_company_data_account" :autosave="3000"></entity-field>
-        </div>
-
-        <div class="col-6">
-            <entity-field :entity="entity" prop="payment_company_data_account_dv" :autosave="3000"></entity-field>
-        </div>
-    </div>
-</article>

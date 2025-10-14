ad71874036ba934c539e53e3cc31e8ead50128c8
Rafa Chaves <rafael.chaves.freitas@gmail.com>
2025-10-14 10:19:33 -0300

    evita quebra nos db-updates

--- layouts/parts/registration/registration-payment-form.php
+++ layouts/parts/registration/registration-payment-form.php
@@ -1,5 +1,7 @@
 <?php
-$this->import('registration-payment-form');
+if(!$this instanceof \MapasCulturais\Theme) {
+    return;
+}
 ?>
 
 <registration-payment-form :step="step" :entity="registration"></registration-payment-form>
